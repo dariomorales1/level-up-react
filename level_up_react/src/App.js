ea05d239e19d0e05a58ecf0cea24bbe71bf74988
razrd88 <144981608+razrd88@users.noreply.github.com>
2025-10-07 21:04:43 -0300

    reordenamiento de estructura de proyecto

--- level_up_react/src/App.js
+++ level_up_react/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
 import Contacto from './pages/Contacto';
 import Garantia from './pages/Garantia';
 import Terminos from './pages/Terminos';
