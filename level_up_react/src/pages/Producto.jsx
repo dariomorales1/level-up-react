3f9c3e8eeebf071e7e16424c90eca420b8a83352
dariomorales1 <dar.moralesa@duocuc.cl>
2025-10-11 17:34:35 -0300

    Creacion de la pagina de Detalles de los productos

--- level_up_react/src/pages/Producto.jsx
+++ level_up_react/src/pages/Producto.jsx
@@ -1,14 +1,78 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
+import '../styles/pages/productoStyles.css';
+import listaProductos from '../assets/listaProductos';
+import renderEstrellas from '../components/stars';
 
 const Producto = () => {
-    const { id } = useParams();
+    const [searchParams] = useSearchParams();
+    const codigo = searchParams.get('codigo');
+
+    const producto = listaProductos.find(p => 
+        p["Código"] === codigo || p.id === codigo
+    );
+
+    
     
     return (
-        <div className="container mt-4">
-            <h1>Detalle del Producto #{id}</h1>
-            {/* Migrar contenido de producto.html aquí */}
-        </div>
+        <main>
+            <div className="container fluid">
+                <div className="row">
+                    <div className="col-1"></div>
+                    <div className="col-10">
+                        <section className="productDetalle">
+                            <div className="cardDetalle">
+                                <img src={require(`../${producto.imgLink}`)} alt={producto.Nombre} />
+                                <div className="productInfo">
+                                    <h2>{producto.Nombre}</h2>
+                                    <p>{producto['Descripción Larga']}</p>
+                                    <section className='compra'>
+                                        <p className='precio'><strong>Precio: $</strong>{producto.Precio}</p>
+                                        <button className="btnAgregar">Añadir al carrito</button>
+                                    </section>
+                                    </div>
+                            </div>
+                            <hr />
+                            
+                            {renderEstrellas(producto.Puntuacion)}
+
+                            <hr />
+                            <div className="detalles">
+                                <table className='especificaciones'>
+                                    <thead>
+                                        <tr>
+                                            <th><strong>Especificaciones</strong></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {producto.Especificaciones.map((espec, index) => (
+                                            <tr key={index}>
+                                                <td>{espec}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                                <table className='comentarios'>
+                                    <thead>
+                                        <tr>
+                                            <th><strong>Comentarios</strong></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {producto.Comentarios.map((coments, index) => (
+                                            <tr key={index}>
+                                                <td>{coments}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </section>
+                    </div>
+                    <div className="col-1"></div>
+                </div>
+            </div>
+        </main>
     );
 };
 
